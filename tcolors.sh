a61c0843094517c209bef24f9ccdcb49526fc6b4
Elstig <4122785+lestig@users.noreply.github.com>
2020-05-12 23:32:07 -0700

    Delete tcolors.sh

--- tcolors.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-fail_color=$'\033[31;1m'
-color_end=$'\033[0m'
-function="foo"
-line_number="42"
-
-printf "%sError - Function: %s, Line: %d%s\n" "$fail_color" "$function" "$line_number" "$color_end"
